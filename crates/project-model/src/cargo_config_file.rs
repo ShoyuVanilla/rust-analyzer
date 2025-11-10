fd2e4973e09a7ca223b724fa5b221e779737e98a
Shoyu Vanilla <modulo641@gmail.com>
2025-11-11 03:54:42 +0900

    fix: Parse cargo config files with origins

--- crates/project-model/src/cargo_config_file.rs
+++ crates/project-model/src/cargo_config_file.rs
@@ -1,5 +1,5 @@
-//! Read `.cargo/config.toml` as a JSON object
-use paths::{AbsPathBuf, Utf8Path, Utf8PathBuf};
+//! Read `.cargo/config.toml` as a TOML object
+use paths::{AbsPath, Utf8Path, Utf8PathBuf};
 use rustc_hash::FxHashMap;
 use toml::{
     Spanned,
@@ -9,16 +9,11 @@ use toolchain::Tool;
 
 use crate::{ManifestPath, Sysroot, utf8_stdout};
 
-pub(crate) type CargoConfigFile = serde_json::Map<String, serde_json::Value>;
+#[derive(Clone)]
+pub struct CargoConfigFile(String);
 
-pub(crate) struct CargoConfigFileReader<'a> {
-    toml_str: &'a str,
-    line_ends: Vec<usize>,
-    toml: Spanned<DeTable<'a>>,
-}
-
-impl<'a> CargoConfigFileReader<'a> {
-    pub(crate) fn read(
+impl CargoConfigFile {
+    pub(crate) fn load(
         manifest: &ManifestPath,
         extra_env: &FxHashMap<String, Option<String>>,
         sysroot: &Sysroot,
@@ -32,44 +27,52 @@ impl<'a> CargoConfigFileReader<'a> {
         }
 
         tracing::debug!("Discovering cargo config by {cargo_config:?}");
-        let toml_str = utf8_stdout(&mut cargo_config)
+        utf8_stdout(&mut cargo_config)
             .inspect(|toml| {
                 tracing::debug!("Discovered cargo config: {toml:?}");
             })
             .inspect_err(|err| {
                 tracing::debug!("Failed to discover cargo config: {err:?}");
             })
-            .ok()?;
-        let toml = DeTable::parse(&toml_str)
+            .ok()
+            .map(CargoConfigFile)
+    }
+
+    pub(crate) fn read<'a>(&'a self) -> Option<CargoConfigFileReader<'a>> {
+        CargoConfigFileReader::new(&self.0)
+    }
+
+    #[cfg(test)]
+    pub(crate) fn from_string_for_test(s: String) -> Self {
+        CargoConfigFile(s)
+    }
+}
+
+pub(crate) struct CargoConfigFileReader<'a> {
+    toml_str: &'a str,
+    line_ends: Vec<usize>,
+    table: Spanned<DeTable<'a>>,
+}
+
+impl<'a> CargoConfigFileReader<'a> {
+    fn new(toml_str: &'a str) -> Option<Self> {
+        let toml = DeTable::parse(toml_str)
             .inspect_err(|err| tracing::debug!("Failed to parse cargo config into toml: {err:?}"))
             .ok()?;
         let line_ends = toml_str.lines().fold(vec![], |mut acc, l| {
-            acc.push(acc.last().copied().unwrap_or(0_usize) + l.len());
+            acc.push(acc.last().copied().unwrap_or(0_usize) + l.len() + 1);
             acc
         });
 
-        todo!()
-        // Some(CargoConfigFile2 { toml_str, toml, line_ends })
+        Some(CargoConfigFileReader { toml_str, table: toml, line_ends })
     }
 
-    pub(crate) fn new(toml_str: &'a str) -> Option<Self> {
-        let toml = DeTable::parse(&toml_str)
-            .inspect_err(|err| tracing::debug!("Failed to parse cargo config into toml: {err:?}"))
-            .ok()?;
-        let line_ends = toml_str.lines().fold(vec![], |mut acc, l| {
-            acc.push(acc.last().copied().unwrap_or(0_usize) + l.len());
-            acc
-        });
-
-        Some(CargoConfigFileReader { toml_str, toml, line_ends })
-    }
-
-    fn get_spanned(
+    pub(crate) fn get_spanned(
         &self,
         accessor: impl IntoIterator<Item = &'a str>,
     ) -> Option<&Spanned<DeValue<'a>>> {
         let mut keys = accessor.into_iter();
-        let mut val = self.toml.get_ref().get(keys.next()?)?;
+        let mut val = self.table.get_ref().get(keys.next()?)?;
         for key in keys {
             let DeValue::Table(map) = val.get_ref() else { return None };
             val = map.get(key)?;
@@ -78,64 +81,49 @@ impl<'a> CargoConfigFileReader<'a> {
     }
 
     pub(crate) fn get(&self, accessor: impl IntoIterator<Item = &'a str>) -> Option<&DeValue<'a>> {
-        self.get_spanned(accessor).map(Spanned::get_ref)
+        self.get_spanned(accessor).map(|it| it.as_ref())
     }
 
-    pub(crate) fn get_with_origin(
-        &'a self,
-        accessor: impl IntoIterator<Item = &'a str>,
-    ) -> Option<(&'a DeValue<'a>, Option<AbsPathBuf>)> {
-        let val = self.get_spanned(accessor)?;
-        let span = val.span();
-        for &end in &self.line_ends {
-            if end < span.end {
+    pub(crate) fn get_origin_root(&self, spanned: &Spanned<DeValue<'a>>) -> Option<&AbsPath> {
+        let span = spanned.span();
+
+        for &line_end in &self.line_ends {
+            if line_end < span.end {
                 continue;
             }
 
-            // table.key = "value" # [ROOT]/home/.cargo/config.toml
+            let after_span = &self.toml_str[span.end..line_end];
+
+            // table.key = "value" # /parent/.cargo/config.toml
             //                   |                                |
             //                   span.end                         end
-            let origin_path =
-                &self.toml_str[span.end..end].trim_start().strip_prefix('#').and_then(|path| {
+            let origin_path = after_span
+                .strip_prefix([',']) // strip trailing comma
+                .unwrap_or(after_span)
+                .trim_start()
+                .strip_prefix(['#'])
+                .and_then(|path| {
                     let path = path.trim();
-                    if path.starts_with("environment variable") { None } else { Some(path) }
+                    if path.starts_with("environment variable")
+                        || path.starts_with("--config cli option")
+                    {
+                        None
+                    } else {
+                        Some(path)
+                    }
                 });
 
-            return Some((
-                val.get_ref(),
-                origin_path.and_then(|path| Utf8PathBuf::from(path).try_into().ok()),
-            ));
+            return origin_path.and_then(|path| {
+                <&Utf8Path>::from(path)
+                    .try_into()
+                    .ok()
+                    .and_then(AbsPath::parent)
+                    .and_then(AbsPath::parent)
+            });
         }
 
         None
     }
-}
-
-pub(crate) fn read(
-    manifest: &ManifestPath,
-    extra_env: &FxHashMap<String, Option<String>>,
-    sysroot: &Sysroot,
-) -> Option<CargoConfigFile> {
-    let mut cargo_config = sysroot.tool(Tool::Cargo, manifest.parent(), extra_env);
-    cargo_config
-        .args(["-Z", "unstable-options", "config", "get", "--format", "json"])
-        .env("RUSTC_BOOTSTRAP", "1");
-    if manifest.is_rust_manifest() {
-        cargo_config.arg("-Zscript");
-    }
-
-    tracing::debug!("Discovering cargo config by {:?}", cargo_config);
-    let json: serde_json::Map<String, serde_json::Value> = utf8_stdout(&mut cargo_config)
-        .inspect(|json| {
-            tracing::debug!("Discovered cargo config: {:?}", json);
-        })
-        .inspect_err(|err| {
-            tracing::debug!("Failed to discover cargo config: {:?}", err);
-        })
-        .ok()
-        .and_then(|stdout| serde_json::from_str(&stdout).ok())?;
-
-    Some(json)
 }
 
 pub(crate) fn make_lockfile_copy(
@@ -160,22 +148,54 @@ pub(crate) fn make_lockfile_copy(
 }
 
 #[test]
-fn foobar() {
-    let s = r##"
-alias.codegen = "run --package xtask --bin xtask -- codegen" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-alias.dist = "run --package xtask --bin xtask -- dist" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-alias.lint = "clippy --all-targets -- --cap-lints warn" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-alias.qt = "tq" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-alias.tq = "test -- -q" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-alias.xtask = "run --package xtask --bin xtask --" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-env.CARGO_WORKSPACE_DIR.relative = true # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-env.CARGO_WORKSPACE_DIR.value = "" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-env.foo = [
-    "x", # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
-    "yz", # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
+fn cargo_config_file_reader_works() {
+    let toml = r##"
+alias.foo = "abc"
+alias.bar = "🙂" # /ROOT/home/.cargo/config.toml
+alias.sub-example = [
+    "sub", # /ROOT/foo/.cargo/config.toml
+    "example", # /ROOT/bar/.cargo/config.toml
 ]
-target.x86_64-pc-windows-msvc.linker = "rust-lld" # /home/shoyu/projects/rust-analyzer-git/.cargo/config.toml
+build.rustflags = [
+    "--flag", # /ROOT/home/.cargo/config.toml
+    "env", # environment variable `CARGO_BUILD_RUSTFLAGS`
+    "cli", # --config cli option
+]
+env.CARGO_WORKSPACE_DIR.relative = true # /ROOT/home/.cargo/config.toml
+env.CARGO_WORKSPACE_DIR.value = "" # /ROOT/home/.cargo/config.toml
 "##;
-    let x = DeTable::parse(s).unwrap();
-    dbg!(&x);
+    let reader = CargoConfigFileReader::new(toml).unwrap();
+
+    let alias_foo = reader.get_spanned(["alias", "foo"]).unwrap();
+    assert_eq!(alias_foo.as_ref().as_str().unwrap(), "abc");
+    assert!(reader.get_origin_root(alias_foo).is_none());
+
+    let alias_bar = reader.get_spanned(["alias", "bar"]).unwrap();
+    assert_eq!(alias_bar.as_ref().as_str().unwrap(), "🙂");
+    assert_eq!(reader.get_origin_root(alias_bar).unwrap().as_str(), "/ROOT/home");
+
+    let alias_sub_example = reader.get_spanned(["alias", "sub-example"]).unwrap();
+    assert!(reader.get_origin_root(alias_sub_example).is_none());
+    let alias_sub_example = alias_sub_example.as_ref().as_array().unwrap();
+
+    assert_eq!(alias_sub_example[0].get_ref().as_str().unwrap(), "sub");
+    assert_eq!(reader.get_origin_root(&alias_sub_example[0]).unwrap().as_str(), "/ROOT/foo");
+
+    assert_eq!(alias_sub_example[1].get_ref().as_str().unwrap(), "example");
+    assert_eq!(reader.get_origin_root(&alias_sub_example[1]).unwrap().as_str(), "/ROOT/bar");
+
+    let build_rustflags = reader.get(["build", "rustflags"]).unwrap().as_array().unwrap();
+    assert_eq!(reader.get_origin_root(&build_rustflags[0]).unwrap().as_str(), "/ROOT/home");
+    assert!(reader.get_origin_root(&build_rustflags[1]).is_none());
+    assert!(reader.get_origin_root(&build_rustflags[2]).is_none());
+
+    let env_cargo_workspace_dir =
+        reader.get(["env", "CARGO_WORKSPACE_DIR"]).unwrap().as_table().unwrap();
+    let env_relative = &env_cargo_workspace_dir["relative"];
+    assert!(env_relative.as_ref().as_bool().unwrap());
+    assert_eq!(reader.get_origin_root(env_relative).unwrap().as_str(), "/ROOT/home");
+
+    let env_val = &env_cargo_workspace_dir["value"];
+    assert_eq!(env_val.as_ref().as_str().unwrap(), "");
+    assert_eq!(reader.get_origin_root(env_val).unwrap().as_str(), "/ROOT/home");
 }
